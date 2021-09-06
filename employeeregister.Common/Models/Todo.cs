c7826b447edf7d8eb13226453c59a39d468755ae
bbedotor <53658555+bbedotor@users.noreply.github.com>
2021-09-06 11:27:15 -0500

    Entity

---
+++ employeeregister.Common/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace employeeregister.Common.Models
+{
+    public class Todo
+    {
+        public DateTime createTime { get; set; }
+        public int Type { get; set; }
+        public bool consolidated { get; set; }
+    }
+}
---
+++ employeeregister.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace employeeregister.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime createTime { get; set; }
+        public int Type { get; set; }
+        public bool consolidated { get; set; }
+    }
+}
