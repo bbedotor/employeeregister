dc4a3ab4df97b7cf84f37804079266c0cd87c84e
BRAYAN BEDOYA TORO <brayanbedoya190814@correo.itm.edu.co>
2021-09-06 21:59:05 -0500

    local setting

---
+++ employeeregister.Test/Test/ScheduledFunctionTest.cs
@@ -0,0 +1,29 @@
+﻿using employeeregister.Functions.Functions;
+using employeeregister.Test.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace employeeregister.Test.Test
+{
+    public class ScheduledFunctionTest
+    {
+        [Fact]
+        public void ScheduledFunction_Should_Log_Message()
+        {
+            // Arrange
+            MockCloudTableRegisters mockTodos = new MockCloudTableRegisters(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            // Act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains("Deleting completed", message);
+        }
+
+    }
+}
+
